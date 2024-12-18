0d2625b1da1a308addfec221884d18083ee4791d
Fiqqar <145265758+Fiqqar@users.noreply.github.com>
2024-12-18 20:08:10 +0700

    18-12-24 (adding raw-loader)
    done
    -adding raw-loader for fetching api/data from API

---
+++ src/components/Landing.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <button @click="Hello World">Hello World</button>
+    </div>
+</template>
+
+<script>
+import textFile from '!!raw-loader!./file.txt';
+let arr;
+export default {
+    name: 'LandingPage',
+    data() {
+        return {
+            arr: textFile
+        }
+    }
+}
+</script>
