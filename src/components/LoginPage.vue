cde97300365fad867c78ad008bd7afeb3a35c338
Fiqqar <145265758+Fiqqar@users.noreply.github.com>
2024-12-19 18:00:28 +0700

    19-12-24 (fix bug)
    done
    - fix bug for button animation hover and active

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -57,7 +57,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .animate {
     transition: all 0.3s ease;
 }
--- src/components/RegisterPage.vue
+++ src/components/RegisterPage.vue
@@ -2,7 +2,7 @@
     <div class="d-flex justify-content-center align-items-center vh-100" style="background-color: #f4fdf4;">
         <div class="card shadow-sm" style="width: 24rem; border-radius: 10px;">
             <div class="card-body">
-                <h3 class="card-title text-center mb-4">Register</h3>
+                <h3 class="card-title text-center mb-4">Sign Up</h3>
                 <form>
                     <div class="mb-3">
                         <label for="name" class="form-label">Full Name</label>
@@ -24,11 +24,11 @@
                         <input type="password" id="confirmPassword" class="form-control"
                             placeholder="Confirm password" required>
                     </div>
-                    <button type="submit" class="btn btn-success w-100">Register</button>
+                    <button type="submit" class="btn btn-success w-100 animate">Register</button>
                 </form>
                 <div class="text-center mt-3">
-                    <p class="">Already have an account? <router-link to="/login"
-                            class="text-success text-decoration-none">Sign in here</router-link></p>
+                    <p id="login1">Already have an account? </p><router-link to="/login"
+                            class="text-success text-decoration-none" id="login">Sign in here</router-link>
                 </div>
             </div>
         </div>
@@ -50,18 +50,17 @@ export default {
 
 <style scoped>
 #login1 {
-    color: white;
+    color: black;
     text-decoration: none;
     font-size: 14px;
-    margin-top: 10px;
+    margin-top: 5px;
     display: inline-block;
     margin-bottom: 10px;
     margin-left: 2px;
+    margin-right: 5px;
 }
 
 #login {
-    color: white;
-    text-decoration: underline;
     font-size: 14px;
     display: inline-block;
     transition: color 0.3s ease-in-out;
