47302d6d47f73f0561dda62485b650a991d27f92
fiq <145265758+Fiqqar@users.noreply.github.com>
2024-12-29 16:23:49 +0700

    29-12-24 (HomePage, and ProfilePage)
    done

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'font-awesome/css/font-awesome.min.css';
+import '@fontsource/quicksand';
 
 const app = createApp(App)
 app.use(router)
